b9b8ffbd8efc3fb204d59df5dd57b03349379a5f
lucid-k <maiyokimmant@gmail.com>
2025-06-19 21:29:45 +0300

    display text and use of placeholders

---
+++ printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("Hello ! My name is %s\n","Anthony");
+	printf("I am %d old and %g inches tall\n",30,5.11);
+	printf("The character %c has the ASCII value %d\n",'A','A');
+	return(0);
+}
